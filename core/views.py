6b5b6e3a590045e4c940425f98bf1c37a24e5150
Hiba Alaani <55714121+hibaalaani@users.noreply.github.com>
2025-06-26 17:43:30 +0200

    Update views.py
    Import EmailMessage

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
+from django.core.mail import send_mail , EmailMessage
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Topic, Project, Booking , ContactMessage , CustomUser
